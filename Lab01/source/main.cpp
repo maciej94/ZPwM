e0e91c4d0964e6595bcd277f4eee779c342961a1
maciej94 <maciej22771100@gmail.com>
2017-03-13 18:13:59 +0100

    Skonczony program.

--- Lab01/source/main.cpp
+++ Lab01/source/main.cpp
@@ -1,41 +1,26 @@
 #include <Windows.h> //zawsze dolaczany do aplikacji windowsowej
+#include <stdio.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) //dla aplikacji win zamiast main jest WinMain
 {
   int iRetKey = MessageBox(0, "Wybierz liczbe z zakresu 1-40.", "Gra", MB_OK);
+  
+  int gora = 40, dol = 1, srodek = 0;
+  char buf[500];
 
-  if (iRetKey == IDOK)
+  do
   {
-    iRetKey = MessageBox(0, "Czy liczba jest wieksza od 20?", "Gra", MB_YESNO);
-    if (iRetKey == IDYES)
-    {
-      iRetKey = MessageBox(0, "Czy liczba jest wieksza od 30?", "Gra", MB_YESNO);
-      if (iRetKey == IDYES)
-      {
-        iRetKey = MessageBox(0, "Czy liczba jest wieksza od 35?", "Gra", MB_YESNO);
-        if (iRetKey == IDYES)
-        {
-          iRetKey = MessageBox(0, "Czy liczba jest wieksza od 38?", "Gra", MB_YESNO);
-          if (iRetKey == IDYES)
-          {
-            iRetKey = MessageBox(0, "Czy liczba jest wieksza od 39?", "Gra", MB_YESNO);
-            if (iRetKey == IDYES)
-            {
-              MessageBox(0, "Twoja liczba to 40", "Gra", MB_YESNO);
-            }
-            else MessageBox(0, "Twoja liczba to 39", "Gra", MB_YESNO);
-          }
-          else
-          {
+    srodek = (gora + dol) / 2;
+    sprintf_s(buf, "Czy Twoja liczba jest wieksza od %d?", srodek);
+    iRetKey = MessageBox(0, buf, "Gra", MB_YESNO);
+    if (iRetKey == IDYES) dol = srodek + 1;
+    else gora = srodek;
+  } while (gora > dol);
+  
+  sprintf_s(buf, "Twoja liczba to %d?", dol);
+  iRetKey = MessageBox(0, buf, "Gra", MB_YESNO);
+  if (iRetKey == IDYES) MessageBox(0, "Zgadlem!", "Gra", MB_OK);
+  else MessageBox(0, "UPS!", "Gra", MB_OK);
 
-          }
-        }
-      }
-    }
-  }
-  else
-  {
-    MessageBox(0, "Smutno", "Aplikacja", MB_OK | MB_ICONQUESTION);
-  }
   return 0;
 }
